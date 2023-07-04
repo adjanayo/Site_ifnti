3391bf6a5658fc85dd6dd03f89be95ed8544af95
LangoeGithub <fahouzia.langoe@ifnti.com>
2023-07-04 15:52:37 +0000

    class_map, footer_adresses

--- resources/views/vitrine/pages/contact.blade.php
+++ resources/views/vitrine/pages/contact.blade.php
@@ -49,7 +49,7 @@
                 <div class="col-xl-6 col-md-12 col-sm-12">
                     <div class="map-box">
                         {{-- <div id="custom-places" class="small-map"></div> --}}
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15764.01890553856!2d1.1313802!3d8.9717103!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10294e7eba23be49%3A0xe66697f7c9ad4e52!2sIFNTI!5e0!3m2!1sfr!2sus!4v1684854560055!5m2!1sfr!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width="600" height="450"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15764.01890553856!2d1.1313802!3d8.9717103!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10294e7eba23be49%3A0xe66697f7c9ad4e52!2sIFNTI!5e0!3m2!1sfr!2sus!4v1684854560055!5m2!1sfr!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width="600" height="450" class="small-map"></iframe>
                     </div>
                 </div>
             </div>
